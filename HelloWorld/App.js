48f0ee93397ee647b56cbe318ddeed0a7e388dab
justinfmorgan <justinfmorgan@gmail.com>
2020-04-12 17:56:01 -0700

    Finished the hello world, added to README

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function HelloWorldApp() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello, world!</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default HelloWorldApp;
